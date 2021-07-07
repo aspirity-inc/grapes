5c4d4e324bcf7425e328ac5bf00619913388071b
Denis Zavershinskiy <zaverden@gmail.com>
2021-07-07 14:13:36 +0700

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb-typescript/base",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
